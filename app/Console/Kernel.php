fda28d68485b62e8169fb5fce1044d36b2767695
Bimeri Noel <bimerinoel@gmail.com>
2025-05-01 22:57:14 +0300

    Modify the readme file and change scheduler for 1 hour

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,6 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('emails:parse')->everyMinute()->withoutOverlapping();
+        $schedule->command('emails:parse')->hourly()->withoutOverlapping();
     }
 
     /**
